65c2781de38438c7f520a8ca0bb1dcb15f3479c1
Dathimo Ronde <dathimo@gmail.com>
2023-03-12 15:51:20 +0100

    created by toegevoegd

--- database/migration/v0/2023_03_05_03_create_user_info_tables.sql
+++ database/migration/v0/2023_03_05_03_create_user_info_tables.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS user_info (
 	user_id INTEGER PRIMARY KEY references users(id),
 	full_name VARCHAR(100) NOT NULL DEFAULT "",
 	IBAN VARCHAR(34) NOT NULL DEFAULT "",
-	BIC VARCHAR(12) NOT NULL DEFAULT ""
+	BIC VARCHAR(12) NOT NULL DEFAULT "",
+	created_by INTEGER references users(id)
 );
 
 COMMIT;
